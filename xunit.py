46fa672c90169095f978fe85b5dda3ce7fb6526c
Eduardo G Cardoso <eduardo.gcardoso1@gmail.com>
2024-05-21 22:29:12 -0300

    tdd chapter 21 counting: no idea what happened but the tests are not failing

--- xunit.py
+++ xunit.py
@@ -5,10 +5,13 @@ class TestCase:
     def setUp(self):
         pass
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return result
     def tearDown(self):
         pass
 
@@ -27,6 +30,16 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+    def testBrokenMethod(self):
+        raise Exception
+
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
@@ -34,5 +47,13 @@ class TestCaseTest(TestCase):
         test = WasRun("testMethod")
         test.run()
         assert("setUp testMethod tearDown " == test.log)
+    def testResult(self):
+        test = WasRun("testMethod")
+        result = test.run()
+        assert("1 run, 0 failed" == result.summary())
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert "1 run, 1 failed", result.summary
 
 TestCaseTest("testTemplateMethod").run()
