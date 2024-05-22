029cb7d18a067d3efd4b47649d5fdeebf2031b81
Eduardo G Cardoso <eduardo.gcardoso1@gmail.com>
2024-05-21 22:19:56 -0300

    tdd 20 cleaning up after: now I understand that assert() in python prints out an error if the assertion is false

--- xunit.py
+++ xunit.py
@@ -8,6 +8,9 @@ class TestCase:
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
+    def tearDown(self):
+        pass
 
 class WasRun(TestCase):
     def __init__(self, name) -> None:
@@ -17,17 +20,19 @@ class WasRun(TestCase):
     def setUp(self):
         self.wasRun = None
         self.wasSetUp = 1
+        self.log = "setUp "
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
+    def tearDown(self):
+        self.log = self.log + "tearDown "
 
 class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
-    def testRunning(self):
-        self.test.run()
-        assert(self.test.wasRun)
-    def testSetUp(self):
-        self.test.run()
-        assert(self.test.wasSetUp)
+    def testTemplateMethod(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert("setUp testMethod tearDown " == test.log)
 
-TestCaseTest("testRunning").run()
+TestCaseTest("testTemplateMethod").run()
